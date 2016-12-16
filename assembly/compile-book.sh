e46d9409f81c3db84175f75825dadb1d8a8155df
Paul Popoff <oc14@ya.ru>
2016-12-17 00:29:00 +0300

    script that compiles the book was added

---
+++ assembly/compile-book.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# todo: create help command
+# todo: select between ru and eng versions
+
+# the whole file
+all_chapters=""
+
+for chapter in ../ru/textbook/*/ ; do
+  chapter_text=`eval cat $chapter*`
+  all_chapters="$all_chapters\n\n$chapter_text"
+done
+
+#echo "$all_chapters"
+
+pandoc --latex-engine=xelatex -s -o "book.html" <<< "$all_chapters"
+
