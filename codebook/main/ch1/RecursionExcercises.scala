ee148bc200e2aed6f0027c22a8bf5801231efd1c
Paul Popoff <oc14@ya.ru>
2017-01-09 07:05:29 +0300

    A template for recursion task was implemented

---
+++ codebook/main/ch1/RecursionExcercises.scala
@@ -0,0 +1,23 @@
+package scalacourse
+package ch1
+
+
+object RecursionExercises {
+
+  // substringCount("catcowcat", "cat") -> 2
+  // substringCount("catcowcat", "c")   -> 3
+  // substringCount("abracadabra", "z") -> 0
+  // substringCount("aaaaa", "aa") -> 4
+  def substringCount(s: String, sub: String): Int = {
+    ???
+  }
+
+
+  def balance(chars: List[Char]): Boolean = {
+    // defined an inner function that will be used
+    // for recursion
+    false
+  }
+
+}
+
