7c152620f429bf8c6803098ced70780102995488
Paul Popoff <oc14@ya.ru>
2017-01-09 07:12:06 +0300

    assignment text was added

--- codebook/main/ch1/DiskUsage.scala
+++ codebook/main/ch1/DiskUsage.scala
@@ -1,9 +1,19 @@
-package chr1
-package patmatch
+package scalacourse
+package ch1
 
 
-object Main {
+// Here we want you to implement an utility that counts a disk space usage for
+// given directory, including all nested files/directories. The program should
+// have command line interface. It should accept a directory which size will be
+// counted as the only command line argument. If there's no parameter given, the
+// program should count disk usage for the current directory. If it is not
+// possible to read the file, the program should print an error message with
+// their name on the `stdin`.
+object App {
+  def main(args: Array[String]): Unit = ()
+}
 
 
-  def main(args: Array[String): Unit = ()
+object DiskUsage {
+
 }
--- codebook/main/ch1/RecursionExcercises.scala
+++ codebook/main/ch1/RecursionExcercises.scala
@@ -8,16 +8,17 @@ object RecursionExercises {
   // substringCount("catcowcat", "c")   -> 3
   // substringCount("abracadabra", "z") -> 0
   // substringCount("aaaaa", "aa") -> 4
+  // You should an sub-string entrance count for given string.
   def substringCount(s: String, sub: String): Int = {
     ???
   }
 
-
+  // You should write a function that accepts a list of characters and returns
+  // `true` if opening and enclosing parentheses are balanced.
   def balance(chars: List[Char]): Boolean = {
     // defined an inner function that will be used
     // for recursion
     false
   }
-
 }
 
